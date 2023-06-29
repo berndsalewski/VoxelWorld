00ffeb5d45608ac959946d6c2eca2654c2c0e5be
Bernd Salewski <bernd.salewski@gmail.com>
2023-06-29 13:48:14 +0100

    add static SessionGameData for storing global gamedata

---
+++ Assets/Code/VoxelWorld/SessionGameData.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// static global game data which persists between scene loads
+/// </summary>
+static public class SessionGameData
+{
+    static public string worldFileName;
+    internal static bool loadFromFile;
+}
